50d93026adba2a6de223fe6a7569b8fb2e157e54
thenabilarta <thenabilarta@gmail.com>
2022-10-10 01:07:14 +0700

    Initial commit

---
+++ App.tsx
@@ -0,0 +1,53 @@
+import {View, Text} from 'react-native';
+import React, {useEffect} from 'react';
+import {
+  GoogleSignin,
+  GoogleSigninButton,
+  statusCodes,
+} from '@react-native-google-signin/google-signin';
+
+const App = () => {
+  useEffect(() => {
+    GoogleSignin.configure({
+      webClientId:
+        '884998275312-a7uncal6drt9dsbe9ijf8kfri2clgqso.apps.googleusercontent.com',
+      offlineAccess: true,
+      forceCodeForRefreshToken: true,
+    });
+  });
+
+  const signIn = async () => {
+    try {
+      await GoogleSignin.hasPlayServices();
+      const userInfo = await GoogleSignin.signIn();
+
+      console.log(userInfo);
+    } catch (error: any) {
+      if (error.code === statusCodes.SIGN_IN_CANCELLED) {
+        console.log('User cancelled the login flow');
+      } else if (error.code === statusCodes.IN_PROGRESS) {
+        console.log('Signing in');
+      } else if (error.code === statusCodes.PLAY_SERVICES_NOT_AVAILABLE) {
+        console.log('Play services not available');
+      } else {
+        console.log('Some other error happened');
+        console.log(error.message);
+        console.log(error.code);
+      }
+    }
+  };
+
+  return (
+    <View>
+      <Text>App</Text>
+      <GoogleSigninButton
+        style={{width: 192, height: 48, marginTop: 30}}
+        size={GoogleSigninButton.Size.Wide}
+        color={GoogleSigninButton.Color.Dark}
+        onPress={signIn}
+      />
+    </View>
+  );
+};
+
+export default App;
